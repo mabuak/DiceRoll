7b4b492302dd5f651fd0fe661f1122437b2b6a41
Fachruzi Ramadhan <mabuak@live.com>
2018-08-28 18:48:39 +0700

    Add dice roll code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    //Mark : Outlet
+    @IBOutlet var diceImageView1: UIImageView!
+    @IBOutlet var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Mark : Action Button
+    @IBAction func rollButton(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    
 }
 
---
+++ README.md
@@ -0,0 +1,15 @@
+# Dice Roll
+
+Simple project to make dice roll
+
+## Tools
+- Xcode 9.4.1
+- Swift 4
+- Iphone 8
+
+## Screenshot
+![Bingo Login Page](https://github.com/mabuak/dice-roll/blob/master/dice.png)
+
+## Links
+- Apple Developer: [Swift Resources (Xcode 9 + Swift 4)](https://developer.apple.com/swift/resources/)
+
